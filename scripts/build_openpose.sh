4625c0b54acf54564fbcb528b9568ee5f1c55392
ChrisAlthaus <althausc@devbox3.research.tib.eu>
2020-07-01 15:56:19 +0200

    build openpose script cmake install changed 2

--- scripts/build_openpose.sh
+++ scripts/build_openpose.sh
@@ -1,11 +1,17 @@
 #Run in the OpenPose main folder for setting up OpenPose
+# -> sh ../scripts/build_openpose.sh
 
 git submodule update --init --recursive --remote
 
 mkdir build
 cd build
 
-cmake -D BUILD_EXAMPLES=ON  -D BUILD_PYTHON=ON  -D USE_OPENCV=ON  ..
+mkdir bin
+
+cmake --install ../bin -D BUILD_EXAMPLES=ON  -D BUILD_PYTHON=ON  -D USE_OPENCV=ON  ..
 make -j4
-cd python
 make install
+
+
+cd python #optional?
+make -j4 #optional?
--- scripts/mask_rcnn/demo_script.py
+++ scripts/mask_rcnn/demo_script.py
@@ -10,6 +10,14 @@ import matplotlib.pyplot as plt
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../")
 
+#Create output folder for images
+try:
+    os.mkdir(os.path.join(ROOT_DIR, "output"))
+except OSError:
+    print ("Creation of the directory %s failed" % os.path.join(ROOT_DIR, "output"))
+else:
+    print ("Successfully created the directory %s " % os.path.join(ROOT_DIR, "output"))
+
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn import utils
@@ -35,6 +43,8 @@ IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
 
 
+
+
 class InferenceConfig(coco.CocoConfig):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
@@ -46,6 +56,8 @@ config.display()
 
 
 
+
+
 # Create model object in inference mode.
 model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
 
@@ -54,24 +66,21 @@ model.load_weights(COCO_MODEL_PATH, by_name=True)
 
 
 
-# COCO Class names
-# Index of the class in the list is its ID. For example, to get ID of
-# the teddy bear class, use: class_names.index('teddy bear')
-class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-               'bus', 'train', 'truck', 'boat', 'traffic light',
-               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-               'kite', 'baseball bat', 'baseball glove', 'skateboard',
-               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-               'teddy bear', 'hair drier', 'toothbrush']
+
+
+
+
+
+# Load COCO dataset
+dataset = coco.CocoDataset()
+dataset.load_coco(COCO_DIR, "train")
+dataset.prepare()
+
+# Print class names
+print(dataset.class_names)
+
+
+
 
 
 
