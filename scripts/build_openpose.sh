3aa906b044dd984b3132bfba039956df5afe2fc9
Christian Althaus <calthaus@sim.uni-hannover.de>
2020-07-01 15:10:33 +0200

    edited script

--- scripts/build_openpose.sh
+++ scripts/build_openpose.sh
@@ -2,10 +2,24 @@
 
 git submodule update --init --recursive --remote
 
+mkdir bin
 mkdir build
 cd build
 
-cmake -D BUILD_EXAMPLES=ON  -D BUILD_PYTHON=ON  -D USE_OPENCV=ON  ..
+
+cmake --install ../bin -D BUILD_EXAMPLES=ON  -D BUILD_PYTHON=ON  -D USE_OPENCV=ON  ..
 make -j4
-cd python
 make install
+
+"""
+Install the project...
+-- Install configuration: "Release"
+-- Installing: /usr/local/include/openpose
+CMake Error at cmake_install.cmake:46 (file):
+  file INSTALL cannot make directory "/usr/local/include/openpose": Read-only
+  file system.
+
+"""
+
+cd python #optional?
+make -j4 #optional?
