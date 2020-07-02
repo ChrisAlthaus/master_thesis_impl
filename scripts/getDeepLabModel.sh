590e15f2994a33e5264fa1f17e93909b2ef1218c
ChrisAlthaus <althausc@devbox3.research.tib.eu>
2020-07-02 16:13:58 +0200

    deeplab scripts

---
+++ scripts/getDeepLabModel.sh
@@ -0,0 +1,15 @@
+#Loads only the deeplab model from the tensorflow model repository
+#Please run from the root directory
+mkdir deeplab
+cd deeplab
+#git init
+#git config core.sparseCheckout true
+#git remote add -f origin https://github.com/tensorflow/models.git
+#echo "research/deeplab/*" > .git/info/sparse-checkout
+#git checkout master
+
+#pip install --user svn
+echo "SVN checkout might take up to 30 seconds..."
+svn checkout https://github.com/tensorflow/models/trunk/research/deeplab
+echo "checkout done."
+
