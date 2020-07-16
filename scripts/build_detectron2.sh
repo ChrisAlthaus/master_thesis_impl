1970ac5d66158f3f8b5765448edeaae1037ad52d
ChrisAlthaus <althausc@devbox3.research.tib.eu>
2020-07-16 17:46:23 +0200

    build detectron demo & installation

--- scripts/build_detectron2.sh
+++ scripts/build_detectron2.sh
@@ -1,3 +1,7 @@
 # Run this script in the detectron2 base folder
 
-python3 -m pip install -e detectron2
+#python3 -m pip install -e detectron2
+srun -G 1 -w srun -G 1 -w devbox3 --pty singularity run --nv  ../../images/ubuntu_200629.sif \
+python3.6 -m pip install --user detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu102/index.html
+
+mkdir out
